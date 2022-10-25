6429655ffc02e2bc4a0000db3ad57e97e290fe42
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-10-25 10:42:21 +0500

    Add database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+message1 = Message.create([
+  {greeting:"Hello World!"}
+])
+message2 = Message.create([
+  {greeting:"Hello Kim"}
+])
+message3 = Message.create([
+  {greeting:"Hello Chris"}
+])
+
+message4 = Message.create([
+  {greeting:"Hello Silvia"}
+])
+
+message5 = Message.create([
+  {greeting:"Hello John"}
+])
